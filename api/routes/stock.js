47a5f550365fa59c79f32b4fd4b82a80fa551425
sulaimanthealakkas-jpg <sulaimanthealakkas@gmail.com>
2026-07-18 12:26:46 +0300

    Update stock.js

--- api/routes/stock.js
+++ api/routes/stock.js
@@ -1,1 +1,34 @@
 
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+
+    const stock = {
+        lastUpdated: new Date(),
+
+        fruits: [
+            {
+                name: "Leopard",
+                inStock: false,
+                price: 5000000
+            },
+            {
+                name: "Dragon",
+                inStock: false,
+                price: 3500000
+            },
+            {
+                name: "Dough",
+                inStock: true,
+                price: 2800000
+            }
+        ]
+    };
+
+    res.json(stock);
+
+});
+
+module.exports = router;
